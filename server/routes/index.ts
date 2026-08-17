4a0399bebe907ee26a0a01e5d0fb9559c51785e1
emma-nicol <emma.nicol1@justice.gov.uk>
2026-08-17 20:09:40 +0100

    Fixes routing issue with new page (#116)
    * Fixes routing issue with new page
    
    * Update index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -31,6 +31,11 @@ export default function routes({ infoService, auditService }: Services): Router 
   )
 
   router.get('/', asyncHandler(homeController.index))
+
+  router.get('/new-starter-guide', (_req, res) => {
+    res.render('pages/new-starter-guide')
+  })
+
   router.get('/news', asyncHandler(newsController.index))
   router.get('/news/:slug', asyncHandler(newsController.post))
   router.get('/search', asyncHandler(searchController.index))
